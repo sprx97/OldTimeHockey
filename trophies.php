b8954b11ffdc754a560b2f15c7c894eca825660f
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2018-10-01 18:17:52 -0400

    Updated banners on the trophies.php page.

--- trophies.php
+++ trophies.php
@@ -30,32 +30,38 @@
 </script>
 <link href="hof.css" rel="stylesheet"/>
 
-<h5><i>*Last Updated 10/10/16</i></h5>
+<h5><i>*Last Updated 10/1/18</i></h5>
 
 <body><center>
-<div class="header">Division 1 Champions</div>
+<h2><div class="header">Division 1 Champions</div></h2>
 <div>
-	<img src="images/banners/2014D1.png" alt="2014 D1 Champion: Concini" />
-	<img src="images/banners/2015D1.png" alt="2015 D1 Champion: Teratic" />
-	<img src="images/banners/2016D1.png" alt="2016 D1 Champion: Woppa1" />
+	<img src="images/banners/D1/2014.png" alt="2014 D1 Champion: Concini" />
+	<img src="images/banners/D1/2015.png" alt="2015 D1 Champion: Teratic" />
+	<img src="images/banners/D1/2016.png" alt="2016 D1 Champion: Woppa1" />
+	<img src="images/banners/D1/2017.png" alt="2017 D1 Champion: Noddan" />
+	<img src="images/banners/D1/2018.png" alt="2018 D1 Champion: SleepTalkerz" />
 </div>
 
-<div class="header">Points For Champions</div>
+<h2><div class="header">Points For Champions</div></h2>
 <div>
-	<img src="images/banners/2013PF.png" alt="2013 PF Champion: Woppa1" />
-	<img src="images/banners/2014PF.png" alt="2014 PF Champion: Teratic" />
-	<img src="images/banners/2015PF.png" alt="2015 PF Champion: InvisibleTaco" />
-	<img src="images/banners/2016PF.png" alt="2016 PF Champion: Woppa1" />
+	<img src="images/banners/PF/2013.png" alt="2013 PF Champion: Woppa1" />
+	<img src="images/banners/PF/2014.png" alt="2014 PF Champion: Teratic" />
+	<img src="images/banners/PF/2015.png" alt="2015 PF Champion: InvisibleTaco" />
+	<img src="images/banners/PF/2016.png" alt="2016 PF Champion: Woppa1" />
+	<img src="images/banners/PF/2017.png" alt="2017 PF Champion: Coyle1096" />
+	<img src="images/banners/PF/2018.png" alt="2018 PF Champion: SPRX97" />
 </div>
 
-<div class="header">Woppa Cup Champions</div>
+<h2><div class="header">Woppa Cup Champions</div></h2>
 <div>
-	<img src="images/banners/2013WC.png" alt="2013 Woppa Cup Champion: Cannon49" />
-	<img src="images/banners/2014WC.png" alt="2014 Woppa Cup Champion: FCBcn19" />
-	<img src="images/banners/2015WC.png" alt="2015 Woppa Cup Champion: Hkyplyr" />
-	<img src="images/banners/2016WC.png" alt="2016 Woppa Cup Champion: SleepTalkerz" />
+	<img src="images/banners/WC//2013.png" alt="2013 Woppa Cup Champion: Cannon49" />
+	<img src="images/banners/WC/2014.png" alt="2014 Woppa Cup Champion: FCBcn19" />
+	<img src="images/banners/WC/2015.png" alt="2015 Woppa Cup Champion: Hkyplyr" />
+	<img src="images/banners/WC/2016.png" alt="2016 Woppa Cup Champion: SleepTalkerz" />
+	<img src="images/banners/WC/2017.png" alt="2017 Woppa Cup Champion: Boboombang" />
+	<img src="images/banners/WC/2018.png" alt="2018 Woppa Cup Champion: akacesfan" />
 </div>
 
-<h5><i>*Last Updated 10/10/16</i></h5>
+<h5><i>*Last Updated 10/1/18</i></h5>
 
 </center></body>
