d0b790c1b11ab67b7d8019f1b0aa8e1d6c246f19
pnacamuli17 <panac@nacamuli.net>
2019-10-08 18:02:30 -0500

    scroll up button component

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import Homepage from './Homepage';
 import Routes from './Routes';
+import ScrollUpButton from './ScrollUpButton';
 
 function App() {
   return (
     <Router>
       <NavBar />
+      <ScrollUpButton ShowAtPosition={50} />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route component={Routes} />
