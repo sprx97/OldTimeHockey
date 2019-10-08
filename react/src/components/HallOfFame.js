4f89495a0e5e9d87fe311f2d0e212575f735b918
pnacamuli17 <panac@nacamuli.net>
2019-10-08 17:52:03 -0500

    hall of fame page placeholder

--- react/src/components/HallOfFame.js
+++ react/src/components/HallOfFame.js
@@ -1,8 +1,216 @@
 /* eslint-disable */
 import React from 'react';
+import { Container, Grid, Header, Segment, List } from 'semantic-ui-react';
 
 const HallOfFame = () => {
-  return <div>Hall of Fame</div>;
+  return (
+    <Container>
+      <Segment basic>
+        <Header as="h1" textAlign="center" block>
+          Career Regular Season Records
+        </Header>
+        <Grid centered>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Wins
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Winning Percentage
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Points For
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Average Points For
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Coach Rating
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment basic>
+        <Header as="h1" textAlign="center" block>
+          Single Season Records
+        </Header>
+        <Grid centered>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Best Record (Win Pct.)
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Best Record (Wins)
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Points For
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Header as="h2" attached="top">
+                Coach Rating
+              </Header>
+              <Segment attached>
+                <List ordered>
+                  <List.Item as="a">Getting Started</List.Item>
+                  <List.Item as="a">Introduction</List.Item>
+                  <List.Item>
+                    <a>Languages</a>
+                    <List.List>
+                      <List.Item as="a">HTML</List.Item>
+                      <List.Item as="a">Javascript</List.Item>
+                      <List.Item as="a">CSS</List.Item>
+                    </List.List>
+                  </List.Item>
+                  <List.Item as="a">Review</List.Item>
+                </List>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </Container>
+  );
 };
 
 export default HallOfFame;
