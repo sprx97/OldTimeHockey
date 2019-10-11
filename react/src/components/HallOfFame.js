989215c938b4a3fc0c4c85da2e111f3dafa1f1d1
pnacamuli17 <panac@nacamuli.net>
2019-10-10 23:43:20 -0500

    fetch data, create list components and render

--- react/src/components/HallOfFame.js
+++ react/src/components/HallOfFame.js
@@ -1,8 +1,122 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Grid, Header, Segment, List } from 'semantic-ui-react';
 
+const useFetch = url => {
+  const [data, setData] = useState(null);
+
+  async function fetchData() {
+    const response = await fetch(url);
+    const json = await response.json();
+    setData(json.splice(0, 5));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return data;
+};
+
 const HallOfFame = () => {
+  const winsRecord = useFetch('http://www.roldtimehockey.com/node/winsrecord');
+  const winsRecordList =
+    winsRecord &&
+    winsRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.w}
+      </List.Item>
+    ));
+
+  const winPctRecord = useFetch(
+    'http://www.roldtimehockey.com/node/winpctrecord',
+  );
+  const winPctRecordList =
+    winPctRecord &&
+    winPctRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.wpct} ({item.w}-{item.l})
+      </List.Item>
+    ));
+
+  const pfRecord = useFetch('http://www.roldtimehockey.com/node/pfrecord');
+  const pfRecordList =
+    pfRecord &&
+    pfRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.PF}
+      </List.Item>
+    ));
+
+  const avgPfRecord = useFetch(
+    'http://www.roldtimehockey.com/node/avgpfrecord',
+  );
+  const avgPfRecordList =
+    avgPfRecord &&
+    avgPfRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.avg}
+      </List.Item>
+    ));
+
+  const coachRatingRecord = useFetch(
+    'http://www.roldtimehockey.com/node/coachratingrecord',
+  );
+  const coachRatingRecordList =
+    coachRatingRecord &&
+    coachRatingRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.careerCR}% ({item.total} GP)
+      </List.Item>
+    ));
+
+  const seasonWinPctRecord = useFetch(
+    'http://www.roldtimehockey.com/node/seasonwinpctrecord',
+  );
+  const seasonWinPctRecordList =
+    seasonWinPctRecord &&
+    seasonWinPctRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.wpct.toFixed(3)} ({item.wins}-{item.losses}) (
+        {item.year}-{item.year + 1} {item.name})
+      </List.Item>
+    ));
+
+  const seasonWinsRecord = useFetch(
+    'http://www.roldtimehockey.com/node/seasonwinsrecord',
+  );
+  const seasonWinsRecordList =
+    seasonWinsRecord &&
+    seasonWinsRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.wins} ({item.year}-{item.year + 1} {item.name})
+      </List.Item>
+    ));
+
+  const seasonPfRecord = useFetch(
+    'http://www.roldtimehockey.com/node/seasonwinsrecord',
+  );
+  const seasonPfRecordList =
+    seasonPfRecord &&
+    seasonPfRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.pointsFor} ({item.year}-{item.year + 1}{' '}
+        {item.name})
+      </List.Item>
+    ));
+
+  const seasonCoachRatingRecord = useFetch(
+    'http://www.roldtimehockey.com/node/seasoncoachratingrecord',
+  );
+  const seasonCoachRatingRecordList =
+    seasonCoachRatingRecord &&
+    seasonCoachRatingRecord.map(item => (
+      <List.Item key={item.FFname}>
+        {item.FFname} - {item.coachRating.toFixed(2)}% ({item.year}-
+        {item.year + 1} {item.name})
+      </List.Item>
+    ));
+
   return (
     <Container>
       <Segment basic>
@@ -16,19 +130,7 @@ const HallOfFame = () => {
                 Wins
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{winsRecordList}</List>
               </Segment>
             </Grid.Column>
             <Grid.Column>
@@ -36,19 +138,7 @@ const HallOfFame = () => {
                 Winning Percentage (Min. 40 GP)
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{winPctRecordList}</List>
               </Segment>
             </Grid.Column>
           </Grid.Row>
@@ -58,19 +148,7 @@ const HallOfFame = () => {
                 Points For
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{pfRecordList}</List>
               </Segment>
             </Grid.Column>
             <Grid.Column>
@@ -78,19 +156,7 @@ const HallOfFame = () => {
                 Average Points For (Min. 40 GP)
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{avgPfRecordList}</List>
               </Segment>
             </Grid.Column>
           </Grid.Row>
@@ -100,19 +166,7 @@ const HallOfFame = () => {
                 Coach Rating
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{coachRatingRecordList}</List>
               </Segment>
             </Grid.Column>
           </Grid.Row>
@@ -129,19 +183,7 @@ const HallOfFame = () => {
                 Best Record (Win Pct.)
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{seasonWinPctRecordList}</List>
               </Segment>
             </Grid.Column>
             <Grid.Column>
@@ -149,19 +191,7 @@ const HallOfFame = () => {
                 Best Record (Wins)
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{seasonWinsRecordList}</List>
               </Segment>
             </Grid.Column>
           </Grid.Row>
@@ -171,19 +201,7 @@ const HallOfFame = () => {
                 Points For
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{seasonPfRecordList}</List>
               </Segment>
             </Grid.Column>
             <Grid.Column>
@@ -191,19 +209,7 @@ const HallOfFame = () => {
                 Coach Rating
               </Header>
               <Segment attached>
-                <List ordered>
-                  <List.Item as="a">Getting Started</List.Item>
-                  <List.Item as="a">Introduction</List.Item>
-                  <List.Item>
-                    <a>Languages</a>
-                    <List.List>
-                      <List.Item as="a">HTML</List.Item>
-                      <List.Item as="a">Javascript</List.Item>
-                      <List.Item as="a">CSS</List.Item>
-                    </List.List>
-                  </List.Item>
-                  <List.Item as="a">Review</List.Item>
-                </List>
+                <List ordered>{seasonCoachRatingRecordList}</List>
               </Segment>
             </Grid.Column>
           </Grid.Row>
