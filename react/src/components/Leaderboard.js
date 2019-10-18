93d35f019c44fdfa4dc0d7bf5928e4b9cce42e15
pnacamuli17 <panac@nacamuli.net>
2019-10-18 16:48:18 -0500

    add dropdown to leaderboard page

--- react/src/components/Leaderboard.js
+++ react/src/components/Leaderboard.js
@@ -1,13 +1,117 @@
 /* eslint-disable */
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { Container, Segment, Table, Loader } from 'semantic-ui-react';
+import {
+  Container,
+  Segment,
+  Table,
+  Loader,
+  Dropdown,
+  Grid,
+} from 'semantic-ui-react';
 
 export default class Leaderboard extends Component {
   state = {
     column: 'pointsFor',
     data: null,
     direction: 'descending',
+    dropdownOptions: [
+      {
+        key: '2012',
+        text: '2012-2013 Regular Season',
+        value: '2012-2013 Regular Season',
+      },
+      {
+        key: '2012p',
+        text: '2012-2013 Playoffs',
+        value: '2012-2013 Playoffs',
+      },
+      {
+        key: '2013',
+        text: '2013-2014 Regular Season',
+        value: '2013-2014 Regular Season',
+      },
+      {
+        key: '2013p',
+        text: '2013-2014 Playoffs',
+        value: '2013-2014 Playoffs',
+      },
+      {
+        key: '2014',
+        text: '2014-2015 Regular Season',
+        value: '2014-2015 Regular Season',
+      },
+      {
+        key: '2014p',
+        text: '2014-2015 Playoffs',
+        value: '2014-2015 Playoffs',
+      },
+      {
+        key: '2015',
+        text: '2015-2016 Regular Season',
+        value: '2015-2016 Regular Season',
+      },
+      {
+        key: '2015p',
+        text: '2015-2016 Playoffs',
+        value: '2015-2016 Playoffs',
+      },
+      {
+        key: '2016',
+        text: '2016-2017 Regular Season',
+        value: '2016-2017 Regular Season',
+      },
+      {
+        key: '2016p',
+        text: '2016-2017 Playoffs',
+        value: '2016-2017 Playoffs',
+      },
+      {
+        key: '2017',
+        text: '2017-2018 Regular Season',
+        value: '2017-2018 Regular Season',
+      },
+      {
+        key: '2017p',
+        text: '2017-2018 Playoffs',
+        value: '2017-2018 Playoffs',
+      },
+      {
+        key: '2018',
+        text: '2018-2019 Regular Season',
+        value: '2018-2019 Regular Season',
+      },
+      {
+        key: '2018p',
+        text: '2018-2019 Playoffs',
+        value: '2018-2019 Playoffs',
+      },
+      {
+        key: '2019',
+        text: '2019-2020 Regular Season',
+        value: '2019-2020 Regular Season',
+      },
+      {
+        key: '2019p',
+        text: '2019-2020 Playoffs',
+        value: '2019-2020 Playoffs',
+      },
+      {
+        key: 'career',
+        text: 'Career Regular Season',
+        value: 'Career Regular Season',
+      },
+      {
+        key: 'careerp',
+        text: 'Career Playoffs',
+        value: 'Career Playoffs',
+      },
+      {
+        key: 'week',
+        text: 'This Week (Live)',
+        value: 'This Week (Live)',
+      },
+    ],
   };
 
   getData = async () => {
@@ -44,11 +148,26 @@ export default class Leaderboard extends Component {
   };
 
   render() {
-    const { column, data, direction } = this.state;
+    const { column, data, direction, dropdownOptions } = this.state;
 
     return (
       <Container>
         <Segment basic>
+          <Grid centered>
+            <Grid.Row columns={3}>
+              <Grid.Column />
+              <Grid.Column>
+                <Dropdown
+                  fluid
+                  search
+                  selection
+                  options={dropdownOptions}
+                  wrapSelection={false}
+                />
+              </Grid.Column>
+              <Grid.Column />
+            </Grid.Row>
+          </Grid>
           {!data ? (
             <Loader active size="massive" style={{ marginTop: '150px' }} />
           ) : (
