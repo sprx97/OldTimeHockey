b57666605e454b08759fd1a02d7dcfd2a4d79187
pnacamuli17 <panac@nacamuli.net>
2019-10-18 23:37:06 -0500

    split regular season stats table into own component

--- react/src/components/Leaderboard.js
+++ react/src/components/Leaderboard.js
@@ -1,14 +1,8 @@
 /* eslint-disable */
 import _ from 'lodash';
 import React, { Component } from 'react';
-import {
-  Container,
-  Segment,
-  Table,
-  Loader,
-  Dropdown,
-  Grid,
-} from 'semantic-ui-react';
+import { Container, Segment, Dropdown, Grid } from 'semantic-ui-react';
+import RegularSeasonTable from './RegularSeasonTable';
 
 export default class Leaderboard extends Component {
   state = {
@@ -122,7 +116,7 @@ export default class Leaderboard extends Component {
     );
     const leaders = await res.json();
     this.setState({
-      data: leaders,
+      data: _.sortBy(leaders, [this.state.column]).reverse(),
     });
   };
 
@@ -180,129 +174,12 @@ export default class Leaderboard extends Component {
               <Grid.Column />
             </Grid.Row>
           </Grid>
-          {!data ? (
-            <Loader active size="massive" style={{ marginTop: '150px' }} />
-          ) : (
-            <Table definition sortable celled fixed compact>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell width={1} />
-                  <Table.HeaderCell
-                    width={3}
-                    textAlign="center"
-                    sorted={column === 'leaguename' ? direction : null}
-                    onClick={this.handleSort('leaguename')}
-                  >
-                    League
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={6}
-                    textAlign="center"
-                    sorted={column === 'teamname' ? direction : null}
-                    onClick={this.handleSort('teamname')}
-                  >
-                    Team
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={3}
-                    textAlign="center"
-                    sorted={column === 'FFname' ? direction : null}
-                    onClick={this.handleSort('FFname')}
-                  >
-                    User
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={2}
-                    textAlign="center"
-                    sorted={column === 'Wins' ? direction : null}
-                    onClick={this.handleSort('Wins')}
-                  >
-                    Wins
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={2}
-                    textAlign="center"
-                    sorted={column === 'Losses' ? direction : null}
-                    onClick={this.handleSort('Losses')}
-                  >
-                    Losses
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={2}
-                    textAlign="center"
-                    sorted={column === 'pointsFor' ? direction : null}
-                    onClick={this.handleSort('pointsFor')}
-                  >
-                    PF
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={2}
-                    textAlign="center"
-                    sorted={column === 'pointsAgainst' ? direction : null}
-                    onClick={this.handleSort('pointsAgainst')}
-                  >
-                    PA
-                  </Table.HeaderCell>
-                  <Table.HeaderCell
-                    width={2}
-                    textAlign="center"
-                    sorted={column === 'coachRating' ? direction : null}
-                    onClick={this.handleSort('coachRating')}
-                  >
-                    CR%
-                  </Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {_.map(
-                  data,
-                  (
-                    {
-                      leagueID,
-                      leaguename,
-                      teamID,
-                      teamname,
-                      FFname,
-                      Wins,
-                      Losses,
-                      pointsFor,
-                      pointsAgainst,
-                      coachRating,
-                    },
-                    index,
-                  ) => (
-                    <Table.Row key={teamID}>
-                      <Table.Cell textAlign="center">{index + 1}</Table.Cell>
-                      <Table.Cell textAlign="center">
-                        <a
-                          href={`https://www.fleaflicker.com/nhl/leagues/${leagueID}`}
-                          target="_blank"
-                        >
-                          {leaguename}
-                        </a>
-                      </Table.Cell>
-                      <Table.Cell textAlign="center">
-                        <a
-                          href={`https://www.fleaflicker.com/nhl/leagues/${leagueID}/teams/${teamID}`}
-                          target="_blank"
-                        >
-                          {teamname}
-                        </a>
-                      </Table.Cell>
-                      <Table.Cell textAlign="center">{FFname}</Table.Cell>
-                      <Table.Cell textAlign="center">{Wins}</Table.Cell>
-                      <Table.Cell textAlign="center">{Losses}</Table.Cell>
-                      <Table.Cell textAlign="center">{pointsFor}</Table.Cell>
-                      <Table.Cell textAlign="center">
-                        {pointsAgainst}
-                      </Table.Cell>
-                      <Table.Cell textAlign="center">{coachRating}%</Table.Cell>
-                    </Table.Row>
-                  ),
-                )}
-              </Table.Body>
-            </Table>
-          )}
+          <RegularSeasonTable
+            column={column}
+            data={data}
+            direction={direction}
+            handleSort={this.handleSort}
+          />
         </Segment>
       </Container>
     );
