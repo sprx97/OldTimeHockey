5720f7ed429e2d4c9624def2bfa75e9038a81a56
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2019-11-05 02:35:03 -0500

    Prettify !matchup output

--- scripts/DiscordBot/DiscordBot.py
+++ scripts/DiscordBot/DiscordBot.py
@@ -330,7 +330,7 @@ def on_message(message):
 			if len(results) == 0:
 				yield from client.send_message(message.channel, "User " + team + " not found.");
 			else:
-				yield from client.send_message(message.channel, "%s (%d-%d) %0.2f\n%s (%d-%d) %0.2f\nhttps://www.fleaflicker.com/nhl/leagues/%d/scores/%d" % (results[0][0], results[0][6], results[0][7], results[0][1], results[0][2], results[0][8], results[0][9], results[0][3], results[0][4], results[0][5]))
+				yield from client.send_message(message.channel, "%s (%d-%d): **%0.2f**\n%s (%d-%d): **%0.2f**\n<https://www.fleaflicker.com/nhl/leagues/%d/scores/%d>" % (results[0][0], results[0][6], results[0][7], results[0][1], results[0][2], results[0][8], results[0][9], results[0][3], results[0][4], results[0][5]))
 
 			cursor.close()
 			db.close()
