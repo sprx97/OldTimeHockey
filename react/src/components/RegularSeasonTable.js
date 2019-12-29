b964a50c6ed80e71762b97839457a4ff773b4ad6
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2019-12-29 02:36:15 -0500

    Added tiered coloring and hover highlighting to regular season leaderboards.

--- react/src/components/RegularSeasonTable.js
+++ react/src/components/RegularSeasonTable.js
@@ -1,6 +1,46 @@
 import _ from 'lodash';
 import React from 'react';
 import { Container, Segment, Table, Loader } from 'semantic-ui-react';
+import '../styles/App.css';
+
+const divisionMapping = {
+  "Gretzky":"d1",
+  "Eastern":"d2",
+  "Western":"d2",
+  "Roy":"d2",
+  "Hasek":"d2",
+  "Brodeur":"d2",
+  "Price-Murray":"d2",
+  "Jones-Allen":"d2",
+  "Howe":"d3",
+  "Lemieux":"d3",
+  "Dionne":"d3",
+  "Francis":"d3",
+  "Yzerman":"d3",
+  "Jagr":"d3",
+  "Messier":"d3",
+  "Sakic":"d3",
+  "Esposito":"d4",
+  "Recchi":"d4",
+  "Coffey":"d4",
+  "Bourque":"d4",
+  "Pronger":"d4",
+  "Lidstrom":"d4",
+  "Chelios":"d4",
+  "Orr":"d4",
+  "Leetch":"d4",
+  "Niedermayer":"d4",
+}
+
+function highlightLeague(e) {
+  e.target.closest("tr").classList.add("highlight");
+  // also need to find all siblings of the same league (needs to also be a classname)
+  // and highlight them too.
+}
+
+function unhighlightLeague(e) {
+  e.target.closest("tr").classList.remove("highlight");
+}
 
 const RegularSeasonTable = ({ column, data, direction, handleSort }) => {
   handleSort('pointsFor');
@@ -99,9 +139,9 @@ const RegularSeasonTable = ({ column, data, direction, handleSort }) => {
                   },
                   index,
                 ) => (
-                  <Table.Row key={teamID}>
+                  <Table.Row key={teamID} onMouseOver={highlightLeague} onMouseLeave={unhighlightLeague}>
                     <Table.Cell textAlign="center">{index + 1}</Table.Cell>
-                    <Table.Cell textAlign="center">
+                    <Table.Cell textAlign="center" className={divisionMapping[leaguename]}>
                       <a
                         href={`https://www.fleaflicker.com/nhl/leagues/${leagueID}`}
                         target="_blank"
