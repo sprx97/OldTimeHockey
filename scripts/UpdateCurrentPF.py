28d897e3e5a0db9267f056880317cbcb3109eb85
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2020-01-09 16:24:11 -0500

    Fixed updateCurrentPF script based on FF's new UI

--- scripts/UpdateCurrentPF.py
+++ scripts/UpdateCurrentPF.py
@@ -29,16 +29,17 @@ def updateCurrentPF(league, year):
 	else:
 		isProjected = False
 
-	panel = root.cssselect(".panel-body")[0]
+	panel = root.cssselect(".panel-default")[0]
 	if len(root.cssselect(".scoreboard-win")) > 0:
 		isProjected = True
-	for n in xrange(0, len(panel), 2):
-		teamID = panel[n][0].findall("a")[0].get("href")
+	panelrows = panel.findall("tr")
+	for row in panelrows:
+		teamID = row.findall("a")[0].get("href")
 	        if "?season" in teamID:
- 	               teamID = teamID[(teamID.find("/teams/") + 7):teamID.find("?season")]
-     		else:
+	               teamID = teamID[(teamID.find("/teams/") + 7):teamID.find("?season")]
+   		else:
 	               teamID = teamID[(teamID.find("/teams/") + 7):]
-		score = panel[n+1].text_content()
+		score = panel.findall("td")[1].text_content()
 
 		# projections don't count!
 		if isProjected:
