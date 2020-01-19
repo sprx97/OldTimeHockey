2a8a78b9b9e29a9b9a1e33244c80f707def3fc64
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2020-01-19 00:58:26 -0500

    Did some vewy twicky logic to return exactly WHICH trophies to display for each manager. Yay prime factorization :)

--- node/db_connection.js
+++ node/db_connection.js
@@ -97,8 +97,10 @@ http.createServer(function(request, response) {
 			}
 
 			sql = "SELECT FFname, seasons, wins, losses, round(wins/(wins+losses), 3) as pct, round(PF, 2) as PF, round(PF/(wins+losses), 2) as avgPF, round(PA, 2) as PA, \
-			       round(PA/(wins+losses), 2) as avgPA, trophies, careerCR, FFid from (select FFname, count(*) as Seasons, sum(wins) as wins, sum(losses) as losses, sum(pointsFor) as PF, \
-			       sum(pointsAgainst) as PA, sum(isChamp) as trophies, round(100*sum(pointsFor)/sum(100.0*pointsFor/coachRating), 2) as careerCR, FFid \
+			       round(PA/(wins+losses), 2) as avgPA, trophies, careerCR, FFid from (select FFname, count(*) as Seasons, \
+                               sum(wins) as wins, sum(losses) as losses, sum(pointsFor) as PF, sum(pointsAgainst) as PA, \
+                               round(exp(sum(log(CASE WHEN isChamp = 0 THEN 1 WHEN tier = 1 THEN isChamp*7 WHEN tier = 2 THEN isChamp*5 WHEN tier = 3 THEN isChamp*3 WHEN tier = 4 THEN isChamp*2 END)))) as trophies, \
+                               round(100*sum(pointsFor)/sum(100.0*pointsFor/coachRating), 2) as careerCR, FFid \
 			       from Teams inner join Users on ownerID=FFid inner join Leagues on leagueID=Leagues.id where replacement != 1 and pointsFor >=0 " + yearfilter + tierfilter + "group by FFid) as T1 order by PF DESC";
 		}
 		else if (query.year[query.year.length-1] == "p") {
