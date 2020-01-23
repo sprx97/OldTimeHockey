5aa0334c2db243666a6fecd86949c9837f259827
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2020-01-22 23:12:28 -0500

    Automated email sending for playoff odds

--- scripts/PlayoffOdds.py
+++ scripts/PlayoffOdds.py
@@ -1,65 +1,68 @@
 import urllib2 # url reading
 from lxml import etree
 from lxml import html # xml parsing
+import smtplib
+import ssl
+from email.mime.text import MIMEText
 
-link = "http://www.fleaflicker.com/nhl/leagues/11371"
+link = "http://www.fleaflicker.com/nhl/leagues/12090"
 response = urllib2.urlopen(link)
 page = response.read()
 root = html.document_fromstring(page)
 
-print "LeagueBegin"
-print "\tLeague: " + root.cssselect("#top-bar .active")[0].text_content() + " (Sort: League) (Playoffs: 6)"
+body = ""
+
+body += "LeagueBegin\n"
+body += "\tLeague: " + root.cssselect("#top-bar .active")[0].text_content() + " (Sort: League) (Playoffs: 6)\n"
 
 teams = root.cssselect(".league-name a")
 for team in teams:
-	print "\t\tTeam: " + team.text_content()
+	body += "\t\tTeam: " + team.text_content() + "\n"
 
-print "\tKind: fantasy"
-print "\tSport: hockey"
-print "\tSeason 2018 (current: True)"
-print "\tAuthor: Sprx97"
-print "LeagueEnd"
+body += "\tKind: fantasy\n"
+body += "\tSport: hockey\n"
+body += "\tSeason 2018 (current: True)\n"
+body += "\tAuthor: Sprx97\n"
+body += "LeagueEnd\n"
 
-print "SortBegin"
-print "League: Wins, GoalsFor"
-print "SortEnd"
+body += "SortBegin\n"
+body += "League: Wins, GoalsFor\n"
+body += "SortEnd\n"
 
-print "RulesBegin"
-print "\tPointsForWinInRegulation: 1"
-print "\tPointsForWinInOvertime: 1"
-print "\tPointsForWinInShootout: 1"
-print "\tPointsForLossInRegulation: 0"
-print "\tPointsForLossInOvertime: 0"
-print "\tPointsForLossInShootout: 0"
-print "\tPointsForTie: 0.5"
-print "\tPercentOfGamesThatEndInTie: 0"
-print "\tPercentOfGamesThatEndInOvertimeWin: 0"
-print "\tPercentOfGamesThatEndInShootoutWin: 0"
-print "\tHomeFieldAdvantage: 0.5"
-print "\tWeightType: Pythagenpat"
-print "\tWeightExponent: 0.27"
-print "\tWhatDoYouCallATie: tie"
-print "\tWhatDoYouCallALottery: lottery"
-print "\tWhatDoYouCallPromoted: promoted"
-print "\tPromote: 0"
-print "\tPromotePlus: 0"
-print "\tPromotePlusPercent: 0"
-print "\tWhatDoYouCallDemoted: relegated"
-print "\tDemote: 0"
-print "\tDemotePlus: 0"
-print "\tDemotePlusPercent: 0"
-print "RulesEnd"
+body += "RulesBegin\n"
+body += "\tPointsForWinInRegulation: 1\n"
+body += "\tPointsForWinInOvertime: 1\n"
+body += "\tPointsForWinInShootout: 1\n"
+body += "\tPointsForLossInRegulation: 0\n"
+body += "\tPointsForLossInOvertime: 0\n"
+body += "\tPointsForLossInShootout: 0\n"
+body += "\tPointsForTie: 0.5\n"
+body += "\tPercentOfGamesThatEndInTie: 0\n"
+body += "\tPercentOfGamesThatEndInOvertimeWin: 0\n"
+body += "\tPercentOfGamesThatEndInShootoutWin: 0\n"
+body += "\tHomeFieldAdvantage: 0.5\n"
+body += "\tWeightType: Pythagenpat\n"
+body += "\tWeightExponent: 0.27\n"
+body += "\tWhatDoYouCallATie: tie\n"
+body += "\tWhatDoYouCallALottery: lottery\n"
+body += "\tWhatDoYouCallPromoted: promoted\n"
+body += "\tPromote: 0\n"
+body += "\tPromotePlus: 0\n"
+body += "\tPromotePlusPercent: 0\n"
+body += "\tWhatDoYouCallDemoted: relegated\n"
+body += "\tDemote: 0\n"
+body += "\tDemotePlus: 0\n"
+body += "\tDemotePlusPercent: 0\n"
+body += "RulesEnd\n"
 
-print "GamesBegin"
-print "TeamListedFirst: home"
+body += "GamesBegin\n"
+body += "TeamListedFirst: home\n"
 
 for n in xrange(0, 23):
 	weeklink = link + "/scores?week=" + str(n*7 + 4) # weird offset from FF... might change year-to-year
 	weekresponse = urllib2.urlopen(weeklink)
 	weekpage = weekresponse.read()
 	weekroot = html.document_fromstring(weekpage)	
-
-	weekfinished = len(weekroot.cssselect(".scoreboard-win")) == 7 # need 7 wins for the week to be over
 
 	scores = weekroot.cssselect(".scoreboard")
 	for m in xrange(0, len(scores), 2):
@@ -69,13 +72,32 @@ for n in xrange(0, 23):
 		awayname = away.cssselect(".league-name")[0].text_content()
 		homename = home.cssselect(".league-name")[0].text_content()
 
-		if weekfinished:
-			awayscore = away.cssselect(".right")[0].text_content()
-			homescore = home.cssselect(".right")[0].text_content()
-
-			print "1/" + str(n+1) + "/18\t" + homename + "\t" + homescore + "-" + awayscore + "\t" + awayname
+		awayscore = away.cssselect(".right")[0].text_content()
+		homescore = home.cssselect(".right")[0].text_content()
+		if awayscore != "0" and homescore != "0":
+			body += "1/" + str(n+1) + "/18\t" + homename + "\t" + homescore + "-" + awayscore + "\t" + awayname + "\n"
 		else:
-			print "1/" + str(n+1) + "/18\t" + homename + "\t" + "\t" + "\t" + awayname
+			body += "1/" + str(n+1) + "/18\t" + homename + "\t" + "\t" + "\t" + awayname + "\n"
 
 
-print "GamesEnd"
+body += "GamesEnd\n"
+
+#print body
+
+try:
+	msg = MIMEText(body)
+	msg["Subject"] = "playoff odds update"
+	msg["From"] = "no-reply@roldtimehockey.com"
+	recips = ["import@sportsclubstats.com"] #, "jeremy.vercillo@gmail.com"]
+ 	msg["To"] = ",".join(recips)
+
+	s = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+	s.login("roldtimehockey@gmail.com", "OTHMods!");
+	s.sendmail(msg["From"], recips, msg.as_string())
+ 	s.quit()
+
+	print "Email sent."
+
+except Exception as e:
+	print "Error sending email."
+	print e
