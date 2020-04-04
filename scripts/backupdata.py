ef184aaa29b6f1602a862d3117c1c67bb797061f
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2020-04-04 18:00:18 -0400

    Updated database dump

--- scripts/backupdata.py
+++ scripts/backupdata.py
@@ -3,3 +3,4 @@ import os
 
 os.system("mysqldump OldTimeHockey > ../oth.sql -u " + Config.config["sql_username"] + " -p" + Config.config["sql_password"] + " --single-transaction")
 os.system("cp /etc/crontab ../crontab")
+os.system("git commit -a -m 'Updated database dump'")
