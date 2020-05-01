b4aba984e74cee76ea1edcef9a7e27f2fbd05cd3
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2020-05-01 15:50:53 -0400

    Updated instructions and backup script. Added redeploy script to hopefully restart the site when I reboot.

---
+++ deploySite.sh
@@ -0,0 +1,4 @@
+echo "Starting OldTimeHockey react site"
+cd /var/www/OldTimeHockey/react
+serve -s build -l 42426
+
--- scripts/backupdata.py
+++ scripts/backupdata.py
@@ -1,6 +1,6 @@
 import Config
 import os
 
-os.system("mysqldump OldTimeHockey > ../oth.sql -u " + Config.config["sql_username"] + " -p" + Config.config["sql_password"] + " --single-transaction")
-os.system("cp /etc/crontab ../crontab")
+os.system("mysqldump OldTimeHockey > /var/www/OldTimeHockey/oth.sql -u " + Config.config["sql_username"] + " -p" + Config.config["sql_password"] + " --single-transaction")
+os.system("cp /etc/crontab /var/www/OldTimeHockey/crontab")
 os.system("git commit -a -m 'Updated database dump'")
