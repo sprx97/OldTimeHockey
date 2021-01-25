d49ff55cb6e8784b061b61d0d0571bf75bdfad4e
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2021-01-25 14:02:52 -0500

    Updated database dump

--- scripts/backupdata.py
+++ scripts/backupdata.py
@@ -1,6 +1,7 @@
-import Config
 import os
 
-os.system("mysqldump OldTimeHockey Leagues Teams Teams_post Users > /var/www/OldTimeHockey/oth.sql -u " + Config.config["sql_username"] + " -p" + Config.config["sql_password"] + " --single-transaction")
+# --login-path=cronjob was set via mysql_config_editor and includes host=localhost and the same username and password from our config file
+os.system("mysqldump OldTimeHockey Leagues Teams Teams_post Users > /var/www/OldTimeHockey/oth.sql --login-path=cronjob --single-transaction")
 os.system("cp /etc/crontab /var/www/OldTimeHockey/crontab")
+os.system("cd /var/www/OldTimeHockey")
 os.system("git commit -a -m 'Updated database dump'")
