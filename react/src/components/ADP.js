2b9a2a0acd4d914cf16449255c846086a4c00a0e
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2021-02-02 03:13:03 -0500

    Added filter by position to ADP website tab.

--- react/src/components/ADP.js
+++ react/src/components/ADP.js
@@ -1,82 +1,36 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-import { Container, Segment, Dropdown, Divider, Header } from 'semantic-ui-react';
+import { Container, Divider, Dropdown, Grid, Header, Segment } from 'semantic-ui-react';
 import ADPTable from "./ADPTable"
 
 export default class ADP extends Component {
+    dropdownOptions = [
+        {key: "2020", text: "2020-2021", value: "2020"},
+        {key: "2019", text: "2019-2020", value: "2019"},
+        {key: "2018", text: "2018-2019", value: "2018"},
+        {key: "2017", text: "2017-2018", value: "2017"},
+        {key: "2016", text: "2016-2017", value: "2016"},
+        {key: "2015", text: "2015-2016", value: "2015"},
+        {key: "2014", text: "2014-2015", value: "2014"},
+        {key: "2013", text: "2013-2014", value: "2013"},
+        {key: "2012", text: "2012-2013", value: "2012"}]
+    tierOptions = [
+        {key: '1', text: 'D1', value: '1'},
+        {key: '2', text: 'D2', value: '2'},
+        {key: '3', text: 'D3', value: '3'},
+        {key: '4', text: 'D4', value: '4'}]
+    positionOptions = [
+        {key: "C", text: "Center", value: "C"},
+        {key: "LW", text: "Left Wing", value: "LW"},
+        {key: "RW", text: "Right Wing", value: "RW"},
+        {key: "D", text: "Defense", value: "D"},
+        {key: "G", text: "Goalie", value: "G"}]
+
     state = {
         data: null,
         query: "2020",
-        dropdownOptions: [
-            {
-                key: "2020",
-                text: "2020-2021",
-                value: "2020",
-            },
-            {
-                key: "2019",
-                text: "2019-2020",
-                value: "2019",
-            },
-            {
-                key: "2018",
-                text: "2018-2019",
-                value: "2018",
-            },
-            {
-                key: "2017",
-                text: "2017-2018",
-                value: "2017",
-            },
-            {
-                key: "2016",
-                text: "2016-2017",
-                value: "2016",
-            },
-            {
-                key: "2015",
-                text: "2015-2016",
-                value: "2015",
-            },
-            {
-                key: "2014",
-                text: "2014-2015",
-                value: "2014",
-            },
-            {
-                key: "2013",
-                text: "2013-2014",
-                value: "2013",
-            },
-            {
-                key: "2012",
-                text: "2012-2013",
-                value: "2012",
-            },
-        ],
-        tierOptions: [
-            {
-              key: '1',
-              text: 'D1',
-              value: '1',
-            },
-            {
-              key: '2',
-              text: 'D2',
-              value: '2',
-            },
-            {
-              key: '3',
-              text: 'D3',
-              value: '3',
-            },
-            {
-              key: '4',
-              text: 'D4',
-              value: '4',
-            },
-        ],
         tierFilters: null,
+        positionFilters: null,
     };
 
     getData = async() => {
@@ -120,10 +74,6 @@ export default class ADP extends Component {
         this.setState({ query: value, isLoaded: false }, () => this.getData());
     };
 
-    handleTierFilterChange = (event, value) => {
-        this.setState({tierFilters: value.value, isLoaded: false }, () => {this.getData();})
-    }
-
     render() {
         const { data } = this.state;
 
@@ -134,28 +84,47 @@ export default class ADP extends Component {
                         compact
                         search
                         selection
-                        options={this.state.dropdownOptions}
+                        options={this.dropdownOptions}
                         defaultValue={this.state.query}
                         wrapSelection={false}
                         onChange={this.onChange}
                     />
                     <Divider hidden />
-                    <Dropdown
-                        fluid
-                        search
-                        multiple
-                        selection
-                        placeholder="Division(s)"
-                        options={this.state.tierOptions}
-                        wrapSelection={false}
-                        onChange={this.handleTierFilterChange}
-                    />
+                    <Grid centered>
+                        <Grid.Row columns="equal">
+                            <Grid.Column>
+                                <Dropdown
+                                    fluid
+                                    search
+                                    multiple
+                                    selection
+                                    placeholder="Division(s)"
+                                    options={this.tierOptions}
+                                    wrapSelection={false}
+                                    onChange={(event, value) => {this.setState({tierFilters: value.value, isLoaded: false}, () => {this.getData();})}}
+                                />
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Dropdown
+                                    fluid
+                                    search
+                                    multiple
+                                    selection
+                                    placeholder="Position(s)"
+                                    options={this.positionOptions}
+                                    wrapSelection={false}
+                                    onChange={(event, value) => {this.setState({positionFilters: value.value})}}
+                                />
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Segment>
                 {(this.state.isLoaded) ? (<Header as="h4">{this.state.numLeagues}/{this.state.totalLeagues} Completed</Header>) : ("")}
                 <ADPTable
                     data={data}
                     isLoaded={this.state.isLoaded}
                     numLeagues={this.state.numLeagues}
+                    positionFilters={this.state.positionFilters}
                 />
             </Container>
         );
--- react/src/components/ADPTable.js
+++ react/src/components/ADPTable.js
@@ -1,78 +1,103 @@
 import _ from 'lodash';
-import React from 'react';
+import React, {Component} from 'react';
 import { Table, Loader } from 'semantic-ui-react';
 
 var MAX_PICK = 253
 
-const ADPTable = ({data, isLoaded, numLeagues}) => {
-    return (
-        <div>
-            {!isLoaded ? (
-                <Loader active size="massive" style={{ marginTop: '150px' }} />
-            ) : (
-            <Table definition celled compact unstackable>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell width={1} />
-                        <Table.HeaderCell
-                            width={7}
-                            textAlign="center"
-                        >
-                            Player
-                        </Table.HeaderCell>
-                        <Table.HeaderCell
-                            width={2}
-                            textAlign="center"
-                        >
-                            ADP
-                        </Table.HeaderCell>
-                        <Table.HeaderCell
-                            width={2}
-                            textAlign="center"
-                        >
-                            Min
-                        </Table.HeaderCell>
-                        <Table.HeaderCell
-                            width={2}
-                            textAlign="center"
-                        >
-                            Max
-                        </Table.HeaderCell>
-                        <Table.HeaderCell
-                            width={3}
-                            textAlign="center"
-                        >
-                            % Drafted
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                {_.map(
-                    data,
-                    (
-                        {
-                            name,
-                            position,
-                            picks,
-                        },
-                        index,
-                    ) => (
-                    <Table.Row className={position.replace("/", "")}>
-                        <Table.Cell textAlign="center">{index + 1}</Table.Cell>
-                        <Table.Cell textAlign="center">{name}</Table.Cell>
-                        <Table.Cell textAlign="center">{+((picks.reduce((a, b) => a+b) + MAX_PICK*(numLeagues-picks.length)) / numLeagues).toFixed(2)}</Table.Cell>
-                        <Table.Cell textAlign="center">{Math.min.apply(null, picks)}</Table.Cell>
-                        <Table.Cell textAlign="center">{(picks.length === numLeagues) ? Math.max.apply(null, picks) : "--"}</Table.Cell>
-                        <Table.Cell textAlign="center">{Math.floor((100*picks.length/numLeagues)*100)/100}</Table.Cell>
-                    </Table.Row>
-                    ),
+export default class ADPTable extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    CheckPositionFilters(position) {
+        if (this.props.positionFilters == null)
+            return true; // empty filters just means everything
+
+        if (position.includes("C") || position == "F")
+            return this.props.positionFilters.includes("C")
+
+        if (position.includes("LW") || position == "F" || position == "W")
+            return this.props.positionFilters.includes("LW")
+
+        if (position.includes("RW") || position == "F" || position == "W")
+            return this.props.positionFilters.includes("RW")
+
+        if (position.includes("D"))
+            return this.props.positionFilters.includes("D")
+
+        if (position.includes("G"))
+            return this.props.positionFilters.includes("G")
+
+        return false; // Should not reach this
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.props.isLoaded ? (
+                    <Loader active size="massive" style={{ marginTop: '150px' }} />
+                ) : (
+                <Table definition celled compact unstackable>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell width={1} />
+                            <Table.HeaderCell
+                                width={7}
+                                textAlign="center"
+                            >
+                                Player
+                            </Table.HeaderCell>
+                            <Table.HeaderCell
+                                width={2}
+                                textAlign="center"
+                            >
+                                ADP
+                            </Table.HeaderCell>
+                            <Table.HeaderCell
+                                width={2}
+                                textAlign="center"
+                            >
+                                Min
+                            </Table.HeaderCell>
+                            <Table.HeaderCell
+                                width={2}
+                                textAlign="center"
+                            >
+                                Max
+                            </Table.HeaderCell>
+                            <Table.HeaderCell
+                                width={3}
+                                textAlign="center"
+                            >
+                                % Drafted
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                    {_.map(
+                        this.props.data,
+                        (
+                            {
+                                name,
+                                position,
+                                picks,
+                            },
+                            index,
+                        ) => this.CheckPositionFilters(position) ? (
+                        <Table.Row className={position.replace("/", "")}>
+                            <Table.Cell textAlign="center">{index + 1}</Table.Cell>
+                            <Table.Cell textAlign="center">{name}</Table.Cell>
+                            <Table.Cell textAlign="center">{+((picks.reduce((a, b) => a+b) + MAX_PICK*(this.props.numLeagues-picks.length)) / this.props.numLeagues).toFixed(2)}</Table.Cell>
+                            <Table.Cell textAlign="center">{Math.min.apply(null, picks)}</Table.Cell>
+                            <Table.Cell textAlign="center">{(picks.length === this.props.numLeagues) ? Math.max.apply(null, picks) : "--"}</Table.Cell>
+                            <Table.Cell textAlign="center">{Math.floor((100*picks.length/this.props.numLeagues)*100)/100}</Table.Cell>
+                        </Table.Row>
+                        ) : (""),
+                    )}
+                    </Table.Body>
+                </Table>
                 )}
-                </Table.Body>
-            </Table>
-            )}
-        </div>
-    );
+            </div>
+        );
+    }
 };
-
-export default ADPTable;
-
