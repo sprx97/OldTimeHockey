8c77871dfbf5a124b526bebd9005a22604210f07
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2021-02-15 13:15:44 -0500

    Updated backupdata script to only commit relevant files

--- scripts/backupdata.py
+++ scripts/backupdata.py
@@ -4,4 +4,5 @@ import os
 os.system("mysqldump --login-path=cronjob --single-transaction OldTimeHockey Leagues Teams Teams_post Users > /var/www/OldTimeHockey/oth.sql")
 os.system("cp /etc/crontab /var/www/OldTimeHockey/crontab")
 os.system("cd /var/www/OldTimeHockey")
-os.system("git commit -a -m 'Updated database dump'")
+os.system("git add /var/www/OldTimeHockey/crontab /var/www/OldTimeHockey/oth.sql")
+os.system("git commit -m 'Weekly database dump'")
