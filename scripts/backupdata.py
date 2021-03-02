d7736fa1aec547104c3390cdfc089c249de6fe42
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2021-03-02 04:27:38 -0500

    Fixed backupdata script to run correctly from crontab

--- scripts/backupdata.py
+++ scripts/backupdata.py
@@ -3,6 +3,6 @@ import os
 # --login-path=cronjob was set via mysql_config_editor and includes host=localhost and the same username and password from our config file
 os.system("mysqldump --login-path=cronjob --single-transaction OldTimeHockey Leagues Teams Teams_post Users > /var/www/OldTimeHockey/oth.sql")
 os.system("cp /etc/crontab /var/www/OldTimeHockey/crontab")
-os.system("cd /var/www/OldTimeHockey")
+os.chdir("/var/www/OldTimeHockey")
 os.system("git add /var/www/OldTimeHockey/crontab /var/www/OldTimeHockey/oth.sql")
 os.system("git commit -m 'Weekly database dump'")
