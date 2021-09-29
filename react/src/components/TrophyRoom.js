e7ff8a56ba00c433bba7da05ab6815af1d9376a3
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2021-09-29 12:21:01 -0400

    Updated Trophy Room for 2021 winners.

--- react/src/components/TrophyRoom.js
+++ react/src/components/TrophyRoom.js
@@ -14,45 +14,51 @@ const TrophyRoom = () => {
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2014.png"
-                alt="division 1 champion 2014"
+                alt="Division 1 champion 2014"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2015.png"
-                alt="division 1 champion 2015"
+                alt="Division 1 champion 2015"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2016.png"
-                alt="division 1 champion 2016"
+                alt="Division 1 champion 2016"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2017.png"
-                alt="division 1 champion 2017"
+                alt="Division 1 champion 2017"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2018.png"
-                alt="division 1 champion 2018"
+                alt="Division 1 champion 2018"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2019.png"
-                alt="division 1 champion 2019"
+                alt="Division 1 champion 2019"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/D1/2020.png"
-                alt="division 1 champion 2020"
+                alt="Division 1 champion 2020"
               />
               *Regular Season Champion, Playoffs Cancelled Due To Shortened Season
+            </Grid.Column>
+            <Grid.Column>
+              <Image
+                src="/images/banners/D1/2021.png"
+                alt="Division 1 champion 2021"
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -65,49 +71,55 @@ const TrophyRoom = () => {
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2013.png"
-                alt="points for champion 2013"
+                alt="Points for champion 2013"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2014.png"
-                alt="points for champion 2014"
+                alt="Points for champion 2014"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2015.png"
-                alt="points for champion 2015"
+                alt="Points for champion 2015"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2016.png"
-                alt="points for champion 2016"
+                alt="Points for champion 2016"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2017.png"
-                alt="points for champion 2017"
+                alt="Points for champion 2017"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2018.png"
-                alt="points for champion 2018"
+                alt="Points for champion 2018"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2019.png"
-                alt="points for champion 2019"
+                alt="Points for champion 2019"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/PF/2020.png"
-                alt="points for champion 2020"
+                alt="Points for champion 2020"
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Image
+                src="/images/banners/PF/2021.png"
+                alt="Points for champion 2021"
               />
             </Grid.Column>
           </Grid.Row>
@@ -121,54 +133,60 @@ const TrophyRoom = () => {
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2013.png"
-                alt="woppa cup champion 2013"
+                alt="Woppa cup champion 2013"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2014.png"
-                alt="woppa cup champion 2014"
+                alt="Woppa cup champion 2014"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2015.png"
-                alt="woppa cup champion 2015"
+                alt="Woppa cup champion 2015"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2016.png"
-                alt="woppa cup champion 2016"
+                alt="Woppa cup champion 2016"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2017.png"
-                alt="woppa cup champion 2017"
+                alt="Woppa cup champion 2017"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2018.png"
-                alt="woppa cup champion 2018"
+                alt="Woppa cup champion 2018"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2019.png"
-                alt="woppa cup champion 2019"
+                alt="Woppa cup champion 2019"
               />
             </Grid.Column>
             <Grid.Column>
               <Image
                 src="/images/banners/WC/2020.png"
-                alt="woppa cup champion 2020"
+                alt="Woppa cup champion 2020"
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Image
+                src="/images/banners/WC/2021.png"
+                alt="Woppa cup champion 2021"
               />
             </Grid.Column>
           </Grid.Row>
 
-          <Grid.Row columns={4}>Last Updated 6/13/20</Grid.Row>
+          <Grid.Row columns={4}>Last Updated 9/29/21</Grid.Row>
         </Grid>
       </Segment>
     </Container>
