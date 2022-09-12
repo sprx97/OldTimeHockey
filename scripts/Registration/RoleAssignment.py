14a90665943ce2d36b7c3b9b6deb5168176cb33a
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2022-09-12 18:43:22 -0400

    Updated writing of role-assignment script

--- scripts/Registration/RoleAssignment.py
+++ scripts/Registration/RoleAssignment.py
@@ -1,3 +1,36 @@
-# Read registration sheet
-# Get Discord Name and assigned division from sheet
-# Output roles file to config["roles_file_output_location"], a \t separated txt file containing name, discriminator, tier, and league
+# Python includes
+import os
+import sys
+
+# OTH includes
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import Config
+from Emailer import Emailer
+
+# Get all the registrants from the sheet
+sheets_service = Emailer.get_sheets_service()
+sheets = sheets_service.spreadsheets()
+rows = sheets.values().get(spreadsheetId=Config.config["this_season_reg_sheet_id"], range="A:N").execute()
+values = rows.get("values", [])
+
+# Open the output file
+f = open(Config.config["roles_file_out_location"], "w")
+
+DISCORD_NAME_COL = 4 # E
+DIV_ASSIGN_COL = 12 # M
+LEAGUE_ASSIGN_COL = 13 # N
+for row in values[1:]:
+    # Skip managers not assigned to a league yet
+    if len(row) <= LEAGUE_ASSIGN_COL:
+        continue
+
+    # Skip managers without a discord account
+    if row[DISCORD_NAME_COL] == "":
+        continue
+
+    name, discrim = row[DISCORD_NAME_COL].split("#")
+    
+    out_line = f"{name}\t{discrim}\t{row[DIV_ASSIGN_COL]}\t{row[LEAGUE_ASSIGN_COL]}"
+    f.write(out_line)
+
+f.close()
