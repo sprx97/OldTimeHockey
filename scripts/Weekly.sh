b98a605e550fc055f7ef507d929cd1cc96adae47
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2024-03-25 12:03:51 -0400

    Weekly database dump

--- scripts/Weekly.sh
+++ scripts/Weekly.sh
@@ -9,25 +9,29 @@ week=$(head -2 WeekVars.txt | tail -1)
 
 # After week 5, start updating the woppa cup
 if (( $week > 5 )); then
-    python3 /var/www/OldTimeHockey/scripts/woppacup.py true
+#    python3 /var/www/OldTimeHockey/scripts/woppacup.py true
+    echo "WoppaCup Update Complete!"
 fi
 
 # Update the weekly scores tables from fleaflicker
-python3 /var/www/OldTimeHockey/scripts/UpdateTables.py
-python3 /var/www/OldTimeHockey/scripts/PullDailyScores.py
+#python3 /var/www/OldTimeHockey/scripts/UpdateTables.py
+echo "UpdateTables Complete!"
+#python3 /var/www/OldTimeHockey/scripts/PullDailyScores.py
+echo "PullDailyScores Complete!"
 
 # Update the playoff odds json from fleaflicker
 if (( $week > 12 )); then
-    python3 /var/www/OldTimeHockey/scripts/PlayoffOdds/PlayoffOdds_OTH.py
+#    python3 /var/www/OldTimeHockey/scripts/PlayoffOdds/PlayoffOdds_OTH.py
+    echo "PlayoffOdds complete"
 fi
 
 # Generate weekly stat dumps and post to Reddit
-python3 /var/www/OldTimeHockey/scripts/RedditBot/WeeklyStatsPost.py
-python3 /var/www/OldTimeHockey/scripts/RedditBot/PFLeadersPost.py
-python3 /var/www/OldTimeHockey/scripts/RedditBot/RedditPostBot.py
+#python3 /var/www/OldTimeHockey/scripts/RedditBot/WeeklyStatsPost.py
+#python3 /var/www/OldTimeHockey/scripts/RedditBot/PFLeadersPost.py
+#python3 /var/www/OldTimeHockey/scripts/RedditBot/RedditPostBot.py
 
 # Increment week variable
-python3 /var/www/OldTimeHockey/scripts/IncrementWeek.py
+#python3 /var/www/OldTimeHockey/scripts/IncrementWeek.py
 
 # Run weekly database copy
 python3 /var/www/OldTimeHockey/scripts/backupdata.py
