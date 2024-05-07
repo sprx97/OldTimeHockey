e1e451e4fcf2a8d399601d7bf3b5874eb5244eee
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2024-05-06 20:49:20 -0400

    Added new banner

--- react/legacy/src/components/Banner.js
+++ react/legacy/src/components/Banner.js
@@ -5,7 +5,7 @@ import "../styles/App.css"
 const year = "2022";
 
 const Header = () => (
-  <div alt="Banner" style={{width:"100%", height:"200px", backgroundImage:"url('/images/logos/banner.png')", backgroundRepeat:"no-repeat", backgroundPosition:"center top", overflow:"hidden"}}>
+  <div alt="Banner" style={{width:"100%", height:"134px", backgroundImage:"url('/images/logos/banner.webp')", backgroundRepeat:"no-repeat", backgroundPosition:"center top", overflow:"hidden"}}>
   </div>
 );
 
