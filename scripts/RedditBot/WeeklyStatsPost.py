15d54f25ed82953b4c6ebd62eac59f41fa2852f3
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2024-10-31 00:52:54 -0400

    Fixed bug in WeeklyStatsPost. Will be missing 2024 Week 1 and 2, but I'm planning on deprecating this soon anyway.

--- scripts/RedditBot/WeeklyStatsPost.py
+++ scripts/RedditBot/WeeklyStatsPost.py
@@ -4,8 +4,9 @@ import pymysql
 import sys
 
 # My libraries
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import Config
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))) # ./../../
+from shared import Shared
+from shared import Config
 
 f = open(Config.config["srcroot"] + "scripts/WeekVars.txt", "r")
 year = int(f.readline().strip())
