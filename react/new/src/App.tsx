f6b123874565de379b71fb4c2e8f2244c7b61a92
Jeff Scotti <jeffscotti@gmail.com>
2024-11-30 17:08:21 -0800

    Remove double border

--- react/new/src/App.tsx
+++ react/new/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <MantineProvider>
-        <AppShell header={{ height: 60 }} padding='sm'>
+        <AppShell header={{ height: 60 }} padding='sm' withBorder={false}>
           <AppShell.Header>
             <MainNavigation />
           </AppShell.Header>
