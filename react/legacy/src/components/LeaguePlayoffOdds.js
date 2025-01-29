e4451d4c76f26c6cc016a14aac45c959c7022961
Jeff Scotti <jeffscotti@gmail.com>
2025-01-28 18:00:33 -0800

    Styled team basic stats, changed all the charts to bar charts, general cleanup

--- react/legacy/src/components/LeaguePlayoffOdds.js
+++ react/legacy/src/components/LeaguePlayoffOdds.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container, Header, Grid, Dropdown } from 'semantic-ui-react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ScatterChart, Scatter, ZAxis, ResponsiveContainer } from 'recharts';
+import { ComposedChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, LineChart } from 'recharts';
 
 const CustomTooltip = ({ active, payload }) => {
   if (active && payload && payload.length) {
-    const value = payload[0].value;
     return (
-      <div style={{ backgroundColor: 'white', padding: '5px', border: '1px solid #ccc' }}>
-        {typeof value === 'number' ? `${value.toFixed(1)}%` : 'N/A'}
+      <div style={{ backgroundColor: 'white', padding: '10px', border: '1px solid #ccc' }}>
+        {payload.map((entry, index) => (
+          <div key={index} style={{ color: entry.color }}>
+            {entry.name}: {entry.value.toFixed(1)}%
+          </div>
+        ))}
       </div>
     );
   }
@@ -24,6 +27,8 @@ const LeaguePlayoffOdds = (props) => {
     try {
       const response = await fetch(`https://roldtimehockey.com/node/v2/standings/advanced/playoff_odds?league=${leagueId}`);
       const data = await response.json();
+
+      console.log('data', data)
       setPlayoffOdds(data);
       // Set the first team as selected by default
       if (data && Object.keys(data).length > 0) {
@@ -77,8 +82,8 @@ const LeaguePlayoffOdds = (props) => {
   };
 
   return (
-    <Container style={{ marginTop: '2rem' }}>
-      <Header as="h1">{leagueName} - League Playoff Odds</Header>
+    <Container style={{ marginTop: '2rem', padding: '0 1rem' }}>
+      <Header as="h1">{leagueName} - Playoff Odds</Header>
       {playoffOdds && (
         <Dropdown
           placeholder="Select Team"
@@ -91,38 +96,64 @@ const LeaguePlayoffOdds = (props) => {
         />
       )}
       {selectedTeam && (
-        <div>
-
-          <p>Current Record: {selectedTeam.wins}-{selectedTeam.losses}</p>
-          <p>Total PF: {selectedTeam.PF}</p>
-          <p>PF per week average: {selectedTeam.PF_avg}</p>
-          <p>Playoff Odds: {selectedTeam.playoff_odds}%</p>
+       
+        <>
+          <Grid columns={4} style={{ border: '1px solid #ccc', borderRadius: '4px', padding: '15px', width: '100%', maxWidth: '1200px', boxSizing: 'border-box', margin: '0 auto'}} stackable>
+            <Grid.Column>
+              <p style={{textAlign: 'center'}}><strong>Current Record</strong><br/>{selectedTeam.wins}-{selectedTeam.losses}</p>
+            </Grid.Column>
+            <Grid.Column>
+              <p style={{textAlign: 'center'}}><strong>Total PF</strong><br/>{selectedTeam.PF}</p>
+            </Grid.Column>
+            <Grid.Column>
+              <p style={{textAlign: 'center'}}><strong>PF per Week</strong><br/>{selectedTeam.PF_avg.toFixed(1)}</p>
+            </Grid.Column>
+            <Grid.Column>
+              <p style={{textAlign: 'center'}}><strong>Playoff Odds</strong><br/>{selectedTeam.playoff_odds}%</p>
+            </Grid.Column>
+          </Grid>
 
           <Grid columns={2} stackable style={{paddingTop: 25}}>
             <Grid.Row>
               <Grid.Column>
                 <Header as="h3">Playoff Seed Probabilities</Header>
                 <ResponsiveContainer width="100%" height={300}>
-                  <BarChart data={formatSeedData(selectedTeam.seeds)}>
+                  <BarChart 
+                    data={formatSeedData(selectedTeam.seeds)}
+                    margin={{ top: 30, right: 30, bottom: 30, left: 30 }}
+                  >
                   <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="seed" />
-                  <YAxis />
+                  <XAxis 
+                    dataKey="seed"
+                    label={{ value: 'Playoff Seed', position: 'bottom', offset: 0 }}
+                  />
+                  <YAxis 
+                    label={{ value: 'Probability %', angle: -90, position: 'insideLeft' }}
+                  />
                   <Tooltip />
-                  <Legend />
-                  <Bar dataKey="probability" fill="#99D9D9" name="Probability %" />
+                  <Legend verticalAlign="top" height={36} align="right" />
+                  <Bar dataKey="probability" fill="#99D9D9" name="Probability %" barSize={30} />
                   </BarChart>
                 </ResponsiveContainer>
               </Grid.Column>
               <Grid.Column>
                 <Header as="h3">Record-Based Playoff Odds</Header>
                 <ResponsiveContainer width="100%" height={300}>
-                  <BarChart data={formatRecordData(selectedTeam.records)}>
+                  <BarChart 
+                    data={formatRecordData(selectedTeam.records)}
+                    margin={{ top: 30, right: 30, bottom: 30, left: 30 }}
+                  >
                   <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="record" />
-                  <YAxis />
+                  <XAxis 
+                    dataKey="record"
+                    label={{ value: 'Record', position: 'bottom', offset: 0 }}
+                  />
+                  <YAxis 
+                    label={{ value: 'Probability %', angle: -90, position: 'insideLeft' }}
+                  />
                   <Tooltip />
-                  <Legend />
-                  <Bar dataKey="odds" fill="#355464" name="Playoff Odds %" />
+                  <Legend verticalAlign="top" height={36} align="right" />
+                  <Bar dataKey="odds" fill="#355464" name="Playoff Odds %" barSize={30} />
                   </BarChart>
                 </ResponsiveContainer>
               </Grid.Column>
@@ -130,60 +161,49 @@ const LeaguePlayoffOdds = (props) => {
             <Grid.Row>
               <Grid.Column>
                 <Header as="h3">Current Week Impact on Playoff Odds</Header>
-                <ResponsiveContainer width="100%" height={300}>
-                  <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis 
-                    type="category"
-                    dataKey="scenario" 
-                    allowDuplicatedCategory={false}
-                  />
-                  <YAxis 
-                    type="number"
-                    dataKey="odds"
-                    domain={[0, 100]} 
-                    ticks={[0, 20, 40, 60, 80, 100]}
-                    label={{ value: 'Playoff Odds %', angle: -90, position: 'insideLeft' }}
-                  />
-                  <ZAxis range={[400, 400]} />
-                  <Tooltip content={<CustomTooltip />} cursor={{ strokeDasharray: '3 3' }} />
-                  <Legend />
-                  <Scatter 
-                    name="Current" 
-                    data={[{
-                      scenario: 'Current',
-                      odds: selectedTeam.playoff_odds || 0
-                    }]} 
-                    fill="#99D9D9"
-                    shape="circle" 
-                    legendType="circle"
-                  />
-                  <Scatter 
-                    name="Win" 
-                    data={[{
-                      scenario: 'If Win',
-                      odds: selectedTeam.current_week?.win?.odds || 0
-                    }]} 
-                    fill="#355464"
-                    shape="circle"
-                    legendType="circle"
-                  />
-                  <Scatter 
-                    name="Loss" 
-                    data={[{
-                      scenario: 'If Loss',
-                      odds: selectedTeam.current_week?.loss?.odds || 0
-                    }]} 
-                    fill="#E9072B"
-                    shape="circle"
-                    legendType="circle"
-                  />
-                  </ScatterChart>
+                <ResponsiveContainer width="100%" height={350}>
+                  <ComposedChart 
+                    data={[
+                      {
+                        scenario: 'Win',
+                        current: selectedTeam.playoff_odds || 0,
+                        projected: selectedTeam.current_week?.win?.odds || 0
+                      },
+                      {
+                        scenario: 'Loss',
+                        current: selectedTeam.playoff_odds || 0,
+                        projected: selectedTeam.current_week?.loss?.odds || 0
+                      }
+                    ]}
+                    margin={{ top: 30, right: 30, bottom: 30, left: 30 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="scenario" />
+                    <YAxis 
+                      domain={[0, 100]} 
+                      ticks={[0, 20, 40, 60, 80, 100]}
+                      label={{ value: 'Percentage %', angle: -90, position: 'insideLeft' }}
+                    />
+                    <Tooltip content={<CustomTooltip />} />
+                    <Legend verticalAlign="top" height={36} />
+                    <Bar 
+                      dataKey="current" 
+                      fill="#99D9D9"
+                      name="Current Odds"
+                      barSize={30}
+                    />
+                    <Bar 
+                      dataKey="projected" 
+                      fill="#355464"
+                      name="Projected Odds"
+                      barSize={30}
+                    />
+                  </ComposedChart>
                 </ResponsiveContainer>
               </Grid.Column>
             </Grid.Row>
           </Grid>
-        </div>
+     </>
       )}
     </Container>
   );
