ebcb88d6bb3d2da5b2ba2f5e36678be8a3686d27
Jeff Scotti <jeffscotti@gmail.com>
2025-06-20 16:56:59 -0700

    Fixed errors with ReactQueryDevTools, finalized API service layers

--- react/new/src/main.tsx
+++ react/new/src/main.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import ms from 'ms'
 import App from './App.tsx'
 import './index.css'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 3,
-      refetchOnWindowFocus: false,
-      staleTime: ms('5m'),
-    },
-  },
-})
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 )
