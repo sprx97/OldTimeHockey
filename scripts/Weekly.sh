52d29a13eb50a8ffe3d990ccb35e1a1c69abbd38
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2025-12-23 21:46:15 +0000

    Added requirements.txt

--- scripts/Weekly.sh
+++ scripts/Weekly.sh
@@ -34,4 +34,5 @@ echo "WeekVars Incremented!"
 
 # Run weekly database copy
 /var/www/OldTimeHockey/scripts/oth.venv/bin/python3 /var/www/OldTimeHockey/scripts/backupdata.py
+/var/www/OldTimeHockey/scripts/oth.venv/bin/pip freeze > /var/www/OldTimeHockey/scripts/requirements.txt
 echo "Backup Complete!"
