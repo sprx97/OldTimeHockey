796a98162f36160e2436145f0e1cd9975aa39fbc
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2026-05-19 04:15:40 +0000

    Update tables to backup

--- scripts/backupdata.py
+++ scripts/backupdata.py
@@ -1,7 +1,7 @@
 import os
 
 # --login-path=cronjob was set via mysql_config_editor and includes host=localhost and the same username and password from our config file
-os.system("mysqldump --login-path=cronjob --single-transaction OldTimeHockey Leagues Teams Teams_post Users Scoring DraftPicks TeamRegularSeasonScoringTotals > /var/www/OldTimeHockey/oth.sql")
+os.system("mysqldump --login-path=cronjob --single-transaction OldTimeHockey ApiUsageTelemetry DraftPicks Leagues Scoring Teams Teams_post Users > /var/www/OldTimeHockey/oth.sql")
 os.chdir("/var/www/OldTimeHockey")
 os.system("git add /var/www/OldTimeHockey/oth.sql")
 os.system("git add /var/www/OldTimeHockey/scripts/PlayoffOdds/data/*")
