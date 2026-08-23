6a8371e85a4440f79c499bf6d011715638a8ccdd
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2026-08-23 16:20:46 +0000

    Slight tweaks to fleakicker

--- scripts/Registration/Fleakicker.py
+++ scripts/Registration/Fleakicker.py
@@ -3,19 +3,31 @@ import datetime
 import os
 import requests
 import sys
+import time
 
 # OTH includes
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))) # ./../../
 from shared import Shared
 from shared import Config
 
+# NB: Fleaflicker limits us to 90 requests per minute. This script uses a lot.
+#     For each league (15x), we have:
+#     - 1 call to reactivate the league
+#     - 1 call to post the kick message
+#     - 1 call to get the league standings (to get team IDs)
+#     - 14 calls to remove all the teams from the league
+#     - 2 calls to update settings (playoffs and trade deadline)
+#     - 1 call to post the welcome message
+#
+#     Total: 20 calls per league, 15 leagues = 300 calls. Meaning we need a wait after every league.
+
 # Swap this to actually commit changes instead of just printing stuff
-DEBUG = True
+DEBUG = False
 
 # Comment this out to actually run the script.
 # This script is scary so leave this on in case of a fatfinger python command
-print("Aborting due to failsafe")
-quit()
+# print("Aborting due to failsafe")
+# quit()
 
 # Failsafe 1
 print("Are you sure you want to run fleakicker.py? (yes/no)")
@@ -50,15 +62,17 @@ if len(leagues) == 0:
     print(f"No leagues for {year} in database. Ensure WeekVars and DB are correct.")
     quit()
 
-form = "https://forms.gle/zg4s96qHQ7XUMUmA6"
-form_year = 2025
+discord = "https://discord.com/invite/zXTUtj9"
+form = "https://forms.gle/mGznBPwf4KQVPBtt8"
+form_year = 2026
 if form_year != year+1:
     print(f"Form in registration post has not been updated.")
     quit()
 
 # NB: If you want to put spaces into this, use <br> instead of \n
-kick_message = f"OTH {year+1}-{year+2} is about to begin! These leagues are being cleared and new invites will go out shortly. If you're receiving this and haven't signed up, " + \
-               f"please fill out the registration form ({form}) and check the discord/subreddit for more info. ALL RETURNING TEAMS MUST REGISTER. If you have any issues tag @mods on the Discord or DM via reddit for help!"
+kick_message = f"OTH {year+1}-{year+2} is about to begin! These leagues are being cleared and new the new registration is open! If you're receiving this and haven't signed up, " + \
+               f"please fill out the registration form <a href='{form}'>{form}</a> and check <a href='{discord}'>{discord}</a> for more info. " + \
+               f"ALL RETURNING TEAMS MUST REGISTER. If you have any issues tag @mods on Discord for help!"
 kick_message_data = {
     "parentId": "",
     "editId": "",
@@ -72,10 +86,10 @@ season_num = year-2010
 welcome_message = f"<p>Welcome back to OTH for season {season_num}! Draft date/time is FINAL, but " + \
                  "draft order is NOT FINAL. Draft order will be randomized once everyone has joined. " + \
                  "League assignments were made based on availability on the registration form. " + \
-                 "If your availability has changed, tag @mods on <a href='https://discord.com/invite/zXTUtj9'>the discord</a> and we'll try to help.</p>"
+                 f"If your availability has changed, tag @mods on <a href='{discord}'>{discord}</a> and we'll try to help.</p>"
 welcome_message_data = {
     "html": welcome_message,
-    "expiryDate": f"10/07/{year+1}"
+    "expiryDate": f"09/29/{year+1}"
 }
 print(welcome_message_data)
 
@@ -91,14 +105,14 @@ def update_settings(league_id):
         "leagueId": league_id,
         "holdPlayoffs": "true",
         "numPlayoffTeams": 6,
-        "finalsWeek": 26,
+        "finalsWeek": 27, # Week ending 4/4
         "reseed": "false"
     })
     
     # Trade deadline
     session.post("https://www.fleaflicker.com/nhl/editTransactionsSubmit", data={
         "leagueId": league_id,
-        "tradeDeadline": 153, # Monday March 9th, 6am EST
+        "tradeDeadline": 155, # Tuesday March 2nd, 6am EST
         "tradeDeadlineUnlockAfterSeason": "false",
         # addDropDeadline: None
         "addDropDeadlineUnlockAfterSeason": "false",
@@ -113,19 +127,19 @@ def update_settings(league_id):
     })
 
     # Tiebreak rules
-    session.post("https://www.fleaflicker.com/nhl/editTiebreaksSubmit", data={
-        "leagueId": league_id,
-        "gameTiebreak": "STARTER_MAX_CATEGORY,STARTER_MAX_FANTASY_POINTS,BENCH_TOTAL_FANTASY_POINTS",
-        "breakRegularSeasonTies": "false",
-        "rankTiebreak": "POINTS_FOR,HEAD_TO_HEAD,POINTS_AGAINST,STRENGTH_OF_SCHED"
-    })
+    # session.post("https://www.fleaflicker.com/nhl/editTiebreaksSubmit", data={
+    #     "leagueId": league_id,
+    #     "gameTiebreak": "STARTER_MAX_CATEGORY,STARTER_MAX_FANTASY_POINTS,BENCH_TOTAL_FANTASY_POINTS",
+    #     "breakRegularSeasonTies": "false",
+    #     "rankTiebreak": "POINTS_FOR,HEAD_TO_HEAD,POINTS_AGAINST,STRENGTH_OF_SCHED"
+    # })
 
 for league in leagues:
     id = league["id"]
     name = league["name"]
 
     # Skip D1
-    if id == 12086:
+    if id == 12086 or id == 12087 or id == 12088:
         continue
 
     if not DEBUG:
@@ -147,3 +161,5 @@ for league in leagues:
         # Post welcome message to sticky note
         print(f"Adding sticky note to {name}\n")
         session.post("https://www.fleaflicker.com/nhl/leagues/{}/settings/sticky-note".format(id), welcome_message_data)
+
+        time.sleep(30) # Wait 30 seconds to avoid hitting the 90 requests/minute limit
