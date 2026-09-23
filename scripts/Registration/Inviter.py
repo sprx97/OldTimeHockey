e49f5c7aaa4fbb9c6f21512d81743c6a27ec5f6d
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2026-09-23 21:08:36 +0000

    More minor changes

--- scripts/Registration/Inviter.py
+++ scripts/Registration/Inviter.py
@@ -120,7 +120,7 @@ to = "roldtimehockey@gmail.com"
 gmail_service = Emailer.get_gmail_service()
 
 for league_name, emails, invite_link in email_batches:
-    subject = f"OldTimeHockey Invite: {league_name} Division (Accept by 9/20/2026)"
+    subject = f"OldTimeHockey Invite: {league_name} Division (Accept by 9/20/2026 -- LAST CALL)"
     body = \
     "Hello -- \n\n" + \
     "You are receiving this email because you registered for the Old Time Hockey fantasy league this year. " + \
